291e994953034d28146d045a911cea63b4c6db04
勝　駿介 <>
2020-05-23 15:11:51 +0900

    first commit

---
+++ Assets/A/Destroy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+
+    public GameObject ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Invoke("Des", 0.05f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Des()
+    {
+        Destroy(ball);
+
+    }
+}
---
+++ Assets/A/ThrowballScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowballScript : MonoBehaviour
+{
+    public GameObject ThrowingObject;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            ThrowingBall();
+        }
+    }
+
+
+    private void ThrowingBall()
+    {
+        GameObject ball = Instantiate(ThrowingObject, this.transform.position, Quaternion.identity);
+
+        Rigidbody rid = ball.GetComponent<Rigidbody>();
+
+        rid.AddForce(this.transform.forward * rid.mass * 10.0f, ForceMode.Impulse);
+    }
+}
