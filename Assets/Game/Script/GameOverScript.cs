9e1e2d42530f3561e6ab9fa43070e542041a0c9c
勝　駿介 <>
2020-05-26 14:26:43 +0900

    no message

---
+++ Assets/Game/Script/GameOverScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickNewGame()
+    {
+        SceneManager.LoadScene("HomeScene");
+        PlayerPrefs.SetInt("Score", 100);
+        PlayerPrefs.SetInt("Playnumber", 0);
+        PlayerPrefs.SetInt("ScoreUplevel", 1);
+    }
+}
