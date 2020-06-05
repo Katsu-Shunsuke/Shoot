644af36e5325967eb0856dec65fdcbdb39232f77
勝　駿介 <>
2020-06-05 23:44:28 +0900

    no message

---
+++ Assets/Game/Script/LevelScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelScript : MonoBehaviour
+{
+    public Text levelText;
+    public int level;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        level = PlayerPrefs.GetInt("Level");
+
+        if (PlayerPrefs.GetInt("LevelScore") >= Mathf.Pow(2, level) * 1000)
+        {
+            level++;
+            PlayerPrefs.SetInt("Level", level);
+        }
+        
+        levelText.text =( "レベル:" + (PlayerPrefs.GetInt("Level")+1));
+    }
+
+    
+}
---
+++ Assets/Game/Script/ScoreBonusScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBonusScript : MonoBehaviour
+{
+    public int ScoreBonusLevel;
+
+    public Text Buttontext;
+    public Text Nowtext;
+
+    public int levelUpCost;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreBonusLevel = PlayerPrefs.GetInt("ScoreBonusLevel");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Nowtext.text =(PlayerPrefs.GetInt("ScoreBonusLevel")*0.1f)+"%";
+        Buttontext.text = "LEVEL"+PlayerPrefs.GetInt("ScoreBonusLevel")+"\n"+ PlayerPrefs.GetInt("ScoreBonusLevel") * 100 + "円";
+    }
+
+    public void OnClickScoreBonus()
+    {
+        levelUpCost = PlayerPrefs.GetInt("ScoreBonusLevel") * 100;
+        if (levelUpCost < PlayerPrefs.GetInt("Score"))
+        {
+            int t = PlayerPrefs.GetInt("ScoreBonusLevel") + 1;
+            PlayerPrefs.SetInt("ScoreBonusLevel", t);
+            PlayerPrefs.SetInt("Score", PlayerPrefs.GetInt("Score") - levelUpCost);
+        }
+        
+    }
+}
